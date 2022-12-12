f2793e97a158113bed8a12668d5513ff8a1f35d4
Alifend <andres.celis@correounivalle.edu.co>
2022-12-12 00:56:38 -0500

    Custom 404 error page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+import Link from "next/link";
+import { FC } from "react";
+import styled from "styled-components";
+import { OutlinedButton } from "../components/common/Common.styled";
 
-const Error = () => {
-  return <div>Error :(</div>;
+const StyledLink = styled(Link)`
+  position: absolute;
+  left: 100px;
+  bottom: 50px;
+`;
+
+const Container = styled.div`
+  width: 100vw;
+  height: 99vh;
+`;
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+`;
+const Error: FC = () => {
+  return (
+    <Container>
+      <StyledLink href="/">
+        <OutlinedButton>Go back</OutlinedButton>
+      </StyledLink>
+      <Image src="/404.svg" alt="404" />
+    </Container>
+  );
 };
 
 export default Error;
